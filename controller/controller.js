abbbcb110e11d81332842435648b4904543ce2c2
Mahmoud Alrajoula <mahmoud.alra@gmail.com>
2021-01-14 04:24:12 +0100

    kkkk

--- controller/controller.js
+++ controller/controller.js
@@ -5,6 +5,10 @@ const config = require('../config');
 
 mongoose.connect(config.databaseURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+exports.getAllJokes() = function () {
+  return await jokeSchema.find().lean();
+  //forces database to return data in JSOn text format instead of document array
+};
 
 exports.createJoke = function (setup, punchline) {
   return Joke.create({
@@ -20,3 +24,4 @@ exports.getJoke = function (jokeId) {
 exports.getJokes = function () {
   return Joke.find().populate('joke').exec();
 };
+
